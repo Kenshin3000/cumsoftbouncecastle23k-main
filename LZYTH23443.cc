c609e7d6e2e6c456fd8b6b34b42f4f403831cbb6
Cumsoft™ <97250816+cumsoft@users.noreply.github.com>
2023-05-13 19:36:56 -0400

    Create LZYTH23443.cc

---
+++ LZYTH23443.cc
@@ -0,0 +1,46 @@
+//Title of this code
+//Rextester.Program.Main is the entry point for your code. Don't change it.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string a = "DCFC";
+            Console.WriteLine(a);
+            string hex = ConvertStringToHex(a);
+            Console.WriteLine(hex);
+            Console.WriteLine(ConvertHexToString(hex));
+        }
+        
+        public static string ConvertStringToHex(string asciiString)
+        {
+            string hex = "";
+            foreach (char c in asciiString)
+            {
+                int tmp = c;
+                hex += String.Format("{0:x2}", (uint)System.Convert.ToUInt32(tmp.ToString()));
+            }
+            return hex;
+        }
+     
+        public static string ConvertHexToString(string HexValue)
+        {
+            string StrValue = "";
+            while (HexValue.Length > 0)
+            {
+                StrValue += System.Convert.ToChar(System.Convert.ToUInt32(HexValue.Substring(0, 2), 16)).ToString();
+                HexValue = HexValue.Substring(2, HexValue.Length - 2);
+            }
+            return StrValue;
+        }
+    }
+    
+    
+}
