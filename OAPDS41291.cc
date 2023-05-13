b3e604af0d9eec4ed32c9696b4fab7dbb12360f3
Cumsoft™ <97250816+cumsoft@users.noreply.github.com>
2023-05-13 19:20:41 -0400

    Create OAPDS41291.cc

---
+++ OAPDS41291.cc
@@ -0,0 +1,42 @@
+/*
+QUESTION ONE - Power of three
+
+PLEASE READ THIS SECTION IN ITS ENTIRETY!
+
+Given an integer, return whether it is a power of three.
+
+number: 3 -> true
+number: 81 -> true
+number: 5 -> false
+
+Your solution must conform to the following function signature:
+
+bool IsPowerOfThree(int number);
+
+Additional information:
+ - Please include any helper methods that you write as lambdas in IsPowerOfThree(int number). 
+ - Only the content of the IsPowerOfThree(int number) method will be copied from Rextester.
+ - Your code will be run against a MSVC compiler (C++17)
+ - If you need to make any assumptions, please include them as a comment in the method.
+ - Assume that STL is available to be used.
+ - We recommend you write your code in an IDE of your choice (Visual Studio 2019 is preferred, but not required) before pasting your answer down below.
+
+Once you are done, please click "Fork it", and paste the corresponding link into the FlexiQuiz response section associated with this question.
+*/
+
+bool IsPowerOfThree(int number)
+{
+    // Given the number is int, then we assume only positive powers are accepted and not fractions
+    if (number < 1) {
+        return false;
+    }
+    else {
+        // Check if number can be divided by 3
+        // If it can all the way to 1 => it is a power of 3
+        while (number % 3 == 0) {
+             number /= 3;
+        }
+        return number == 1;
+    }
+
+}
